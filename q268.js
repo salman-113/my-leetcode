26298ff6519fc97e8ce94895d869667278e05e5d
salman <mrsalmanxzs@gmail.com>
2025-11-07 10:46:24 +0530

    push

--- q268.js
+++
@@ -1,7 +0,0 @@
-var missingNumber = function (nums) {
-    var n = nums.length;
-    var expectedSum = (n * (n + 1)) / 2;
-    var sum = nums.reduce((a, b) => a + b);
-    return expectedSum - sum;
-};
-console.log(missingNumber([3, 0, 1]));
