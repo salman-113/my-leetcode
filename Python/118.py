c513a06e0941412dc1ab854db737bd151219797f
salman <mrsalmanxzs@gmail.com>
2025-11-20 16:50:27 +0530

    repush

---
+++ Python/118.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        
+        triangle = [[1]]
+
+        for i in range(1, numRows):
+            prev = triangle[-1]
+            row = [1]
+
+            for j in range(1, i):
+                row.append(prev[j-1] + prev[j])
+
+            row.append(1)
+            triangle.append(row)
